3fdd94a8f6c966f3676eac155d17d9f2f4df1b24
Tommi Hännikkälä <tommi.hannikkala@wunderdog.fi>
2020-01-05 15:54:48 +0200

    Added environment variable to bypass tunnel creation for certain environments.

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ Working example of extended image can be found [here](https://github.com/hannikk
 | ----------------------|-----------------------------------------|
 | SSH_USER_AND_HOST     | User name and host of the jump server. In format user@host |
 | SSH_FORWARD_PORTS     | Tunnel forwarded ports. In format *port:remote_host:port*. <br>Example: **5432:postgresql.my.lan:5432** .<br>|
+
+## Technical details
+
+This image extends latest [Alpine](https://alpinelinux.org/) image to keep image as small as possible. [APK](https://wiki.alpinelinux.org/wiki/Alpine_Linux_package_management) is used as package manager. Only installed package in the image is [autossh](https://www.everythingcli.org/ssh-tunnelling-for-fun-and-profit-autossh/) which provides tunneling. Instead of Bash, Alpine uses [Busybox](https://busybox.net/). If you need Bash (or other packages) for your scripts, install them separately. 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env sh
 
-if [ "$SSH_USER_AND_HOST" == "" ]; then
-    echo "Enviroment variable SSH_USER_AND_HOST is not set. Don't know where to set up the tunnel."
-    exit 1
+if [ "$BYPASS_SSH_TUNNEL" = "" ]; then
+  if [ "$SSH_USER_AND_HOST" = "" ]; then
+      echo "Enviroment variable SSH_USER_AND_HOST is not set. Don't know where to set up the tunnel."
+      exit 1
+  fi
+
+  if [ ! -f "/root/.ssh/id_rsa" ] && [ ! -f "/root/.ssh/config" ]; then
+      echo "SSH key file /root/.ssh/id_rsa or /root/.ssh/config does not exist. Cannot start the tunnel."
+      exit 1
+  fi
+
+  FORWARDS=
+
+  for port in $SSH_FORWARD_PORTS; do
+      FORWARDS="$FORWARDS -L $port "
+  done
+
+  autossh -f -T -N -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -oStrictHostKeyChecking=no $FORWARDS $SSH_USER_AND_HOST
+
+  echo "Sleeping for 5 seconds to ensure SSH tunnel has enough time to set up."
+
+  sleep 5
 fi
-
-if [ ! -f "/root/.ssh/id_rsa" ] && [ ! -f "/root/.ssh/config" ]; then
-    echo "SSH key file /root/.ssh/id_rsa or /root/.ssh/config does not exist. Cannot start the tunnel."
-    exit 1
-fi
-
-FORWARDS=
-
-for port in $SSH_FORWARD_PORTS; do
-    FORWARDS="$FORWARDS -L $port "
-done
-
-autossh -f -T -N -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -oStrictHostKeyChecking=no $FORWARDS $SSH_USER_AND_HOST
-
-sleep 5
 
 exec "$@"
